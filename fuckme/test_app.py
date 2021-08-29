c0f85fe32c66bfe265b68a3f61f9e80b2c6ad511
Tom Hill <tomhill98@me.com>
2021-08-29 17:32:24 +1000

    tests!

---
+++ fuckme/test_app.py
@@ -0,0 +1,127 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
+
+from .app import User, app, get_session
+
+
+@pytest.fixture(name="session")
+def session_fixture():
+    # use an in-memory database for testing
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture(name="user")
+def user_fixture(session: Session):
+    user = User(
+        name="Harry Potter",
+        email="harry.potter@hogwarts.magic",
+        password_hash="magic_hashed",
+    )
+    session.add(user)
+    session.commit()
+    return user
+
+
+class TestUsers:
+    def test_create_user(self, client: TestClient):
+        new_user = {
+            "name": "Harry Potter",
+            "email": "h.potter@hogwarts.magic",
+            "password": "wizzzard",
+        }
+        response = client.post(
+            "/users/",
+            json=new_user,
+        )
+        data = response.json()
+
+        assert response.status_code == 200
+        assert data["name"] == new_user["name"]
+        assert data["email"] == new_user["email"]
+        assert data["id"] is not None
+        assert "password" not in data
+
+    def test_create_user_incomplete(self, client: TestClient):
+        response = client.post("/users/", json={"name": "Harry Potter"})
+        assert response.status_code == 422
+
+    def test_create_user_invalid(self, client: TestClient):
+        response = client.post(
+            "/users/", json={"name": "Harry Potter", "email": {}, "password": "magics"}
+        )
+        assert response.status_code == 422
+
+    def test_read_users(self, session: Session, client: TestClient):
+        user_1 = User(
+            name="Harry Potter",
+            email="harry.potter@hogwarts.magic",
+            password_hash="magic_hashed",
+        )
+        user_2 = User(
+            name="Hermione Granger",
+            email="h.granger@hogwarts.magic",
+            password_hash="leviosaaaa_hashed",
+        )
+        session.add(user_1)
+        session.add(user_2)
+        session.commit()
+
+        response = client.get("/users/")
+        data = response.json()
+
+        assert response.status_code == 200
+        assert len(data) == 2
+        assert data[0]["name"] == user_1.name
+        assert data[0]["email"] == user_1.email
+        assert data[0]["id"] == user_1.id
+        assert "password" not in data[0]
+        assert "password_hash" not in data[0]
+
+    def test_read_user(self, user: User, client: TestClient):
+        response = client.get(f"/users/{user.id}")
+        data = response.json()
+
+        assert response.status_code == 200
+        assert data["name"] == user.name
+        assert data["email"] == user.email
+        assert data["id"] == user.id
+        assert "password" not in data
+        assert "password_hash" not in data
+
+    def test_update_user(self, user: User, client: TestClient):
+        response = client.patch(f"/users/{user.id}", json={"name": "Harry Granger"})
+        data = response.json()
+
+        assert response.status_code == 200
+        assert data["name"] == "Harry Granger"
+        assert data["email"] == user.email
+        assert "password" not in data
+        assert "password_hash" not in data
+
+    def test_delete_user(self, user: User, session: Session, client: TestClient):
+
+        response = client.delete(f"/users/{user.id}")
+
+        user_in_db = session.get(User, user.id)
+
+        assert response.status_code == 200
+        assert user_in_db is None
