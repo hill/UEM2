ef6257032b8af2b178ba52b03d17d1d06c6ba07e
Tom Hill <tomhill98@me.com>
2021-09-05 15:55:17 +1000

    better exception logs

--- fuckme/app/core/logger.py
+++ fuckme/app/core/logger.py
@@ -3,13 +3,16 @@ from fastapi.logger import logger
 from rich.logging import RichHandler
 
 
-rich_handler = RichHandler()
+rich_handler = RichHandler(rich_tracebacks=True)
 
 
 def disable_uvicorn_root_logger():
     """Uvicorn adds a default handler to the root logger, so all logs messages are duplicated"""
     uvicorn_logger = logging.getLogger("uvicorn")
     uvicorn_logger.propagate = False
+    # NOTE: can also remove this stuff to just use default handlers
+    uvicorn_logger.handlers = []
+    uvicorn_logger.addHandler(rich_handler)
 
 
 disable_uvicorn_root_logger()
