f08743e6ca1cd5811f463e012fdab1ac26dcb434
Tom Hill <tomhill98@me.com>
2021-09-15 23:55:48 +1000

    kinda cool form styling

--- frontend2/src/views/New.vue
+++ frontend2/src/views/New.vue
@@ -63,7 +63,7 @@
             .toUpperCase();
         }
 
-        const courseName = words.map((word) => word[0].toUpperCase()).join("");
+        const courseName = words.map((word) => word[0]?.toUpperCase()).join("");
         return courseName
           .substring(0, Math.min(4, courseName.length))
           .toUpperCase();
@@ -72,33 +72,49 @@
   };
 </script>
 <template>
-  <div class="block sm:flex flex-row-reverse m-5">
-    <div class="cover flex-1 relative">
-      <div class="sm:w-1/2 sm:fixed p-10">
-        <CourseCard
-          class="mx-auto w-3/4 md:1/2 lg:w-2/5 xl:w-1/3"
-          :code="initials + courseNumber"
-          :name="courseName"
-        />
-        <div class="mt-5 mx-auto text-center">
-          <Button label="Roll" @click="generateCourseNumber()" />
+  <div class="page px-2 md:px-10 lg:px-20 py-10 min-h-screen">
+    <div
+      class="bg-gray-50 block sm:flex flex-row-reverse p-5 rounded-2xl min-h-screen"
+    >
+      <div class="cover flex-1 relative">
+        <div class="sm:w-1/2 sm:fixed p-10">
+          <CourseCard
+            class="mx-auto w-3/4 md:1/2 lg:w-2/5 xl:w-1/3"
+            :code="initials + courseNumber"
+            :name="courseName"
+          />
+          <div class="mt-5 mx-auto text-center">
+            <Button label="Roll" @click="generateCourseNumber()" />
+          </div>
+        </div>
+      </div>
+      <div class="form flex-1">
+        <h1 class="text-xl font-extrabold">New Course</h1>
+        <div class="space-y-8 p-4">
+          <Field label="Course Name" v-model="courseName" />
+          <Field label="Due" type="date" v-model="due" />
+          <Field
+            label="Primary Resource"
+            type="text"
+            v-model="primaryResource"
+          />
+        </div>
+        <h1 class="text-xl font-extrabold">Syllabus</h1>
+        <div class="p-4">
+          <Field label="Example Syllabus" />
+        </div>
+        <h1 class="text-xl font-extrabold">Assessment</h1>
+        <h1 class="text-xl font-extrabold">Motivators</h1>
+        <div class="text-center">
+          <Button @click="save()" label="Create" />
         </div>
       </div>
     </div>
-    <div class="form flex-1">
-      <h1 class="text-xl font-extrabold">New Course</h1>
-      <div class="space-y-8 p-4 lg:w-3/4">
-        <Field label="Course Name" v-model="courseName" />
-        <Field label="Due" type="date" v-model="due" />
-        <Field label="Primary Resource" type="text" v-model="primaryResource" />
-      </div>
-      <h1 class="text-xl font-extrabold">Syllabus</h1>
-      <div class="p-4">
-        <Field label="Example Syllabus" />
-      </div>
-      <h1 class="text-xl font-extrabold">Assessment</h1>
-      <h1 class="text-xl font-extrabold">Motivators</h1>
-      <Button @click="save()" label="Create" />
-    </div>
   </div>
 </template>
+
+<style scoped>
+  .page {
+    background-color: #b2b2b2;
+  }
+</style>
